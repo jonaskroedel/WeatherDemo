d25fc1e8724493c713ce2b5f859ec28504b7963a
jonaskroedel <jonas.kroedel@gmail.com>
2023-02-16 17:45:28 +0100

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Jonas Krödel on 16.02.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
