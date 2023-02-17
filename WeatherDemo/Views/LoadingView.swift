4bb4c6d6d3cd5923b58399502cf098288f11f5cf
jonaskroedel <jonas.kroedel@gmail.com>
2023-02-17 18:30:55 +0100

    Added Layout, Design and Data-fetching

---
+++ WeatherDemo/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  WeatherDemo
+//
+//  Created by Jonas Krödel on 16.02.23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
